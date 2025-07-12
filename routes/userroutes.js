5e85026868eb2a5f15b548ff7a75af90e45645a5
MoritzSchallenberg <schallenbergmoritz@gmail.com>
2025-07-12 23:01:25 +0200

    Update userroutes.js

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const authMiddleware = require('../node_modules/middleware/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
