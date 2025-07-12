df473fa15d9990d67852fd8e17dc2d10fcf801e7
MoritzSchallenberg <schallenbergmoritz@gmail.com>
2025-07-12 22:47:37 +0200

    Add files via upload

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Kein Token vorhanden" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: "Token ungültig" });
+  }
+}
+
+module.exports = authMiddleware;
